639125493c47745c1b1d6ac91200ef0c74bd3b53
Sam <samlam2012@hotmail.com>
2015-06-01 14:59:21 +0100

    Initial Commit

---
+++ htmlparser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  htmlparser
+//
+//  Created by Jiahao Lin on 01/06/2015.
+//  Copyright (c) 2015 Jiahao Lin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
